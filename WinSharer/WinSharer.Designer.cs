f8063340fe196accec81e938c2dfdb22ad49398a
Caroline Yao <cayao@microsoft.com>
2020-07-29 22:46:50 -0700

    apply gitignore, add high dpi support to fix desktop sharing

--- WinSharer/WinSharer.Designer.cs
+++ WinSharer/WinSharer.Designer.cs
@@ -28,88 +28,103 @@ namespace WinSharer
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenXSharePane));
-            this.StartButton = new System.Windows.Forms.Button();
-            this.StopButton = new System.Windows.Forms.Button();
-            this.LogTextBox = new System.Windows.Forms.TextBox();
-            this.SharingModeComboBox = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // StartButton
-            // 
-            this.StartButton.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.StartButton.Location = new System.Drawing.Point(62, 102);
-            this.StartButton.Margin = new System.Windows.Forms.Padding(6);
-            this.StartButton.Name = "StartButton";
-            this.StartButton.Size = new System.Drawing.Size(425, 44);
-            this.StartButton.TabIndex = 0;
-            this.StartButton.Text = "Begin ScreenX Share";
-            this.StartButton.UseVisualStyleBackColor = false;
-            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
-            // 
-            // StopButton
-            // 
-            this.StopButton.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.StopButton.Location = new System.Drawing.Point(503, 102);
-            this.StopButton.Margin = new System.Windows.Forms.Padding(6);
-            this.StopButton.Name = "StopButton";
-            this.StopButton.Size = new System.Drawing.Size(425, 44);
-            this.StopButton.TabIndex = 1;
-            this.StopButton.Text = "Stop Sharing";
-            this.StopButton.UseVisualStyleBackColor = false;
-            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
-            // 
-            // LogTextBox
-            // 
-            this.LogTextBox.Location = new System.Drawing.Point(62, 158);
-            this.LogTextBox.Margin = new System.Windows.Forms.Padding(6);
-            this.LogTextBox.Multiline = true;
-            this.LogTextBox.Name = "LogTextBox";
-            this.LogTextBox.ReadOnly = true;
-            this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.LogTextBox.Size = new System.Drawing.Size(866, 352);
-            this.LogTextBox.TabIndex = 3;
-            // 
-            // SharingModeComboBox
-            // 
-            this.SharingModeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.SharingModeComboBox.FormattingEnabled = true;
-            this.SharingModeComboBox.Location = new System.Drawing.Point(368, 50);
-            this.SharingModeComboBox.Margin = new System.Windows.Forms.Padding(6);
-            this.SharingModeComboBox.Name = "SharingModeComboBox";
-            this.SharingModeComboBox.Size = new System.Drawing.Size(560, 33);
-            this.SharingModeComboBox.TabIndex = 4;
-            this.SharingModeComboBox.SelectedIndexChanged += new System.EventHandler(this.SharingModeComboBox_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(68, 58);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(302, 25);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Select an application to share:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // ScreenXSharePane
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(998, 540);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.SharingModeComboBox);
-            this.Controls.Add(this.LogTextBox);
-            this.Controls.Add(this.StopButton);
-            this.Controls.Add(this.StartButton);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
-            this.Name = "ScreenXSharePane";
-            this.Text = "ScreenX Share Pane";
-            this.Load += new System.EventHandler(this.WinSharer_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenXSharePane));
+			this.StartButton = new System.Windows.Forms.Button();
+			this.StopButton = new System.Windows.Forms.Button();
+			this.LogTextBox = new System.Windows.Forms.TextBox();
+			this.SharingModeComboBox = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// StartButton
+			// 
+			this.StartButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+			this.StartButton.Location = new System.Drawing.Point(41, 61);
+			this.StartButton.Margin = new System.Windows.Forms.Padding(4);
+			this.StartButton.Name = "StartButton";
+			this.StartButton.Size = new System.Drawing.Size(283, 33);
+			this.StartButton.TabIndex = 0;
+			this.StartButton.Text = "Begin ScreenX Share";
+			this.StartButton.UseVisualStyleBackColor = false;
+			this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+			// 
+			// StopButton
+			// 
+			this.StopButton.BackColor = System.Drawing.SystemColors.ScrollBar;
+			this.StopButton.Location = new System.Drawing.Point(335, 61);
+			this.StopButton.Margin = new System.Windows.Forms.Padding(4);
+			this.StopButton.Name = "StopButton";
+			this.StopButton.Size = new System.Drawing.Size(283, 33);
+			this.StopButton.TabIndex = 1;
+			this.StopButton.Text = "Stop Sharing";
+			this.StopButton.UseVisualStyleBackColor = false;
+			this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+			// 
+			// LogTextBox
+			// 
+			this.LogTextBox.Location = new System.Drawing.Point(41, 101);
+			this.LogTextBox.Margin = new System.Windows.Forms.Padding(4);
+			this.LogTextBox.Multiline = true;
+			this.LogTextBox.Name = "LogTextBox";
+			this.LogTextBox.ReadOnly = true;
+			this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.LogTextBox.Size = new System.Drawing.Size(579, 227);
+			this.LogTextBox.TabIndex = 3;
+			// 
+			// SharingModeComboBox
+			// 
+			this.SharingModeComboBox.Dock = System.Windows.Forms.DockStyle.Right;
+			this.SharingModeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.SharingModeComboBox.FormattingEnabled = true;
+			this.SharingModeComboBox.Location = new System.Drawing.Point(238, 0);
+			this.SharingModeComboBox.Margin = new System.Windows.Forms.Padding(4);
+			this.SharingModeComboBox.Name = "SharingModeComboBox";
+			this.SharingModeComboBox.Size = new System.Drawing.Size(341, 24);
+			this.SharingModeComboBox.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(199, 17);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "Select an application to share:";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// panel1
+			// 
+			this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.panel1.Controls.Add(this.SharingModeComboBox);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(41, 26);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(579, 28);
+			this.panel1.TabIndex = 6;
+			// 
+			// ScreenXSharePane
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.ClientSize = new System.Drawing.Size(665, 346);
+			this.Controls.Add(this.LogTextBox);
+			this.Controls.Add(this.StopButton);
+			this.Controls.Add(this.StartButton);
+			this.Controls.Add(this.panel1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "ScreenXSharePane";
+			this.Text = "ScreenX Share Pane";
+			this.Load += new System.EventHandler(this.WinSharer_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -120,6 +135,7 @@ namespace WinSharer
         private System.Windows.Forms.TextBox LogTextBox;
 		private System.Windows.Forms.ComboBox SharingModeComboBox;
         private System.Windows.Forms.Label label1;
-    }
+		private System.Windows.Forms.Panel panel1;
+	}
 }
 
--- WinViewer/WinViewer.Designer.cs
+++ WinViewer/WinViewer.Designer.cs
@@ -34,111 +34,111 @@ namespace WinViewer
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenXViewPane));
-            this.pRdpViewer = new AxRDPCOMAPILib.AxRDPViewer();
-            this.ConnectButton = new System.Windows.Forms.Button();
-            this.DisconnectButton = new System.Windows.Forms.Button();
-            this.ControlButton = new System.Windows.Forms.Button();
-            this.LogTextBox = new System.Windows.Forms.TextBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pRdpViewer)).BeginInit();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // pRdpViewer
-            // 
-            this.pRdpViewer.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
-            this.pRdpViewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pRdpViewer.Enabled = true;
-            this.pRdpViewer.Location = new System.Drawing.Point(0, 142);
-            this.pRdpViewer.Margin = new System.Windows.Forms.Padding(6);
-            this.pRdpViewer.Name = "pRdpViewer";
-            this.pRdpViewer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("pRdpViewer.OcxState")));
-            this.pRdpViewer.Size = new System.Drawing.Size(1348, 1029);
-            this.pRdpViewer.TabIndex = 0;
-            this.pRdpViewer.OnConnectionEstablished += new System.EventHandler(this.OnConnectionEstablished);
-            this.pRdpViewer.OnConnectionFailed += new System.EventHandler(this.OnConnectionFailed);
-            this.pRdpViewer.OnConnectionTerminated += new AxRDPCOMAPILib._IRDPSessionEvents_OnConnectionTerminatedEventHandler(this.OnConnectionTerminated);
-            this.pRdpViewer.OnError += new AxRDPCOMAPILib._IRDPSessionEvents_OnErrorEventHandler(this.OnError);
-            // 
-            // ConnectButton
-            // 
-            this.ConnectButton.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.ConnectButton.Location = new System.Drawing.Point(24, 23);
-            this.ConnectButton.Margin = new System.Windows.Forms.Padding(6);
-            this.ConnectButton.Name = "ConnectButton";
-            this.ConnectButton.Size = new System.Drawing.Size(203, 44);
-            this.ConnectButton.TabIndex = 1;
-            this.ConnectButton.Text = "Connect";
-            this.ConnectButton.UseVisualStyleBackColor = false;
-            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
-            // 
-            // DisconnectButton
-            // 
-            this.DisconnectButton.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.DisconnectButton.Location = new System.Drawing.Point(239, 23);
-            this.DisconnectButton.Margin = new System.Windows.Forms.Padding(6);
-            this.DisconnectButton.Name = "DisconnectButton";
-            this.DisconnectButton.Size = new System.Drawing.Size(209, 44);
-            this.DisconnectButton.TabIndex = 2;
-            this.DisconnectButton.Text = "Disconnect";
-            this.DisconnectButton.UseVisualStyleBackColor = false;
-            this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
-            // 
-            // ControlButton
-            // 
-            this.ControlButton.BackColor = System.Drawing.SystemColors.Info;
-            this.ControlButton.Location = new System.Drawing.Point(24, 79);
-            this.ControlButton.Margin = new System.Windows.Forms.Padding(6);
-            this.ControlButton.Name = "ControlButton";
-            this.ControlButton.Size = new System.Drawing.Size(424, 44);
-            this.ControlButton.TabIndex = 3;
-            this.ControlButton.Text = "Control ScreenX";
-            this.ControlButton.UseVisualStyleBackColor = false;
-            this.ControlButton.Click += new System.EventHandler(this.ControlButton_Click);
-            // 
-            // LogTextBox
-            // 
-            this.LogTextBox.Location = new System.Drawing.Point(479, 27);
-            this.LogTextBox.Margin = new System.Windows.Forms.Padding(6);
-            this.LogTextBox.Multiline = true;
-            this.LogTextBox.Name = "LogTextBox";
-            this.LogTextBox.ReadOnly = true;
-            this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.LogTextBox.Size = new System.Drawing.Size(845, 92);
-            this.LogTextBox.TabIndex = 4;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.DisconnectButton);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(6);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1348, 142);
-            this.panel1.TabIndex = 5;
-            // 
-            // ScreenXViewPane
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(1348, 1171);
-            this.Controls.Add(this.LogTextBox);
-            this.Controls.Add(this.ControlButton);
-            this.Controls.Add(this.ConnectButton);
-            this.Controls.Add(this.pRdpViewer);
-            this.Controls.Add(this.panel1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
-            this.Name = "ScreenXViewPane";
-            this.Text = "ScreenX View Pane";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)(this.pRdpViewer)).EndInit();
-            this.panel1.ResumeLayout(false);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ScreenXViewPane));
+			this.pRdpViewer = new AxRDPCOMAPILib.AxRDPViewer();
+			this.ConnectButton = new System.Windows.Forms.Button();
+			this.DisconnectButton = new System.Windows.Forms.Button();
+			this.ControlButton = new System.Windows.Forms.Button();
+			this.LogTextBox = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pRdpViewer)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pRdpViewer
+			// 
+			this.pRdpViewer.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+			this.pRdpViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pRdpViewer.Enabled = true;
+			this.pRdpViewer.Location = new System.Drawing.Point(0, 91);
+			this.pRdpViewer.Margin = new System.Windows.Forms.Padding(4);
+			this.pRdpViewer.Name = "pRdpViewer";
+			this.pRdpViewer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("pRdpViewer.OcxState")));
+			this.pRdpViewer.Size = new System.Drawing.Size(899, 658);
+			this.pRdpViewer.TabIndex = 0;
+			this.pRdpViewer.OnConnectionEstablished += new System.EventHandler(this.OnConnectionEstablished);
+			this.pRdpViewer.OnConnectionFailed += new System.EventHandler(this.OnConnectionFailed);
+			this.pRdpViewer.OnConnectionTerminated += new AxRDPCOMAPILib._IRDPSessionEvents_OnConnectionTerminatedEventHandler(this.OnConnectionTerminated);
+			this.pRdpViewer.OnError += new AxRDPCOMAPILib._IRDPSessionEvents_OnErrorEventHandler(this.OnError);
+			// 
+			// ConnectButton
+			// 
+			this.ConnectButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+			this.ConnectButton.Location = new System.Drawing.Point(16, 13);
+			this.ConnectButton.Margin = new System.Windows.Forms.Padding(4);
+			this.ConnectButton.Name = "ConnectButton";
+			this.ConnectButton.Size = new System.Drawing.Size(135, 33);
+			this.ConnectButton.TabIndex = 1;
+			this.ConnectButton.Text = "Connect";
+			this.ConnectButton.UseVisualStyleBackColor = false;
+			this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+			// 
+			// DisconnectButton
+			// 
+			this.DisconnectButton.BackColor = System.Drawing.SystemColors.ScrollBar;
+			this.DisconnectButton.Location = new System.Drawing.Point(164, 13);
+			this.DisconnectButton.Margin = new System.Windows.Forms.Padding(4);
+			this.DisconnectButton.Name = "DisconnectButton";
+			this.DisconnectButton.Size = new System.Drawing.Size(135, 33);
+			this.DisconnectButton.TabIndex = 2;
+			this.DisconnectButton.Text = "Disconnect";
+			this.DisconnectButton.UseVisualStyleBackColor = false;
+			this.DisconnectButton.Click += new System.EventHandler(this.DisconnectButton_Click);
+			// 
+			// ControlButton
+			// 
+			this.ControlButton.BackColor = System.Drawing.SystemColors.Info;
+			this.ControlButton.Location = new System.Drawing.Point(16, 51);
+			this.ControlButton.Margin = new System.Windows.Forms.Padding(4);
+			this.ControlButton.Name = "ControlButton";
+			this.ControlButton.Size = new System.Drawing.Size(283, 33);
+			this.ControlButton.TabIndex = 3;
+			this.ControlButton.Text = "Control ScreenX";
+			this.ControlButton.UseVisualStyleBackColor = false;
+			this.ControlButton.Click += new System.EventHandler(this.ControlButton_Click);
+			// 
+			// LogTextBox
+			// 
+			this.LogTextBox.Location = new System.Drawing.Point(319, 13);
+			this.LogTextBox.Margin = new System.Windows.Forms.Padding(4);
+			this.LogTextBox.Multiline = true;
+			this.LogTextBox.Name = "LogTextBox";
+			this.LogTextBox.ReadOnly = true;
+			this.LogTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.LogTextBox.Size = new System.Drawing.Size(565, 71);
+			this.LogTextBox.TabIndex = 4;
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.DisconnectButton);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Margin = new System.Windows.Forms.Padding(4);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(899, 91);
+			this.panel1.TabIndex = 5;
+			// 
+			// ScreenXViewPane
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.ClientSize = new System.Drawing.Size(899, 749);
+			this.Controls.Add(this.LogTextBox);
+			this.Controls.Add(this.ControlButton);
+			this.Controls.Add(this.ConnectButton);
+			this.Controls.Add(this.pRdpViewer);
+			this.Controls.Add(this.panel1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "ScreenXViewPane";
+			this.Text = "ScreenX View Pane";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			((System.ComponentModel.ISupportInitialize)(this.pRdpViewer)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
